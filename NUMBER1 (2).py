e1d78df11700f49b2d4377d6ebe7d0bee5fc31c7
dmarx-web <dmarx2005@mail.ru>
2023-12-12 15:39:52 +0300

    Addввв

--- NUMBER1 (2).py
+++ NUMBER1 (2).py
@@ -1,30 +1,16 @@
-while True:
-    try:
-        A = int(input("Введите значение A : "))
-        break
-    except ValueError:
-        print("Вы ввели некорректное значение. Пожалуйста, введите целое число.")
+def error():
+    print("Введено неправильное значение")
 
 while True:
     try:
-        B = int(input("Введите значение B : "))
+        A = float(input("Введите значение A : "))
+        B = float(input("Введите значение B : "))
+        C = float(input("Введите значение C : "))
+        D = float(input("Введите значение D : "))
         break
-    except ValueError:
-        print("Вы ввели некорректное значение. Пожалуйста, введите целое число.")
+    except:
+        error()
 
-while True:
-    try:
-        C = int(input("Введите значение C : "))
-        break
-    except ValueError:
-        print("Вы ввели некорректное значение. Пожалуйста, введите целое число.")
-
-while True:
-    try:
-        D = int(input("Введите значение D : "))
-        break
-    except ValueError:
-        print("Вы ввели некорректное значение. Пожалуйста, введите целое число.")
 
 def mean(X, Y):
     amean = (X+Y)/2
--- NUMBER2 (2).py
+++ NUMBER2 (2).py
@@ -1,43 +1,32 @@
 import math
+def pr(s):
+    while True:
+        try:
+            r1 = float(input("Введите значение {}: ".format(s)))
+            if r1 > 0:
+                return r1
+                break
+            else:
+                print('Введите значение {} большее нуля: '.format(s))
+                continue
+        except ValueError:
+            print("Вы ввели некорректное значение. Пожалуйста, введите число правильно.")
 
-while True:
-    try:
-        r1 = float(input("Введите значение R1: "))
-        if r1 > 0:
+def pr2(s):
+    while True:
+        try:
+            fi1 = int(input("Введите значение {} : ".format(s)))
+            return fi1
             break
-        else:
-            print('Введите значение R1 большее нуля: ')
-            continue
-    except ValueError:
-        print("Вы ввели некорректное значение. Пожалуйста, введите число правильно.")
+        except ValueError:
+            print("Вы ввели некорректное значение. Пожалуйста, введите целое число.")
 
-while True:
-    try:
-        r2 = float(input("Введите значение R2: "))
-        if r2 > 0:
-            break
-        else:
-            print('Введите значение R2 большее нуля: ')
-            continue
-    except ValueError:
-        print("Вы ввели некорректное значение. Пожалуйста, введите число правильно.")
-
-while True:
-    try:
-        fi1 = int(input("Введите значение φ1 : "))
-        break
-    except ValueError:
-        print("Вы ввели некорректное значение. Пожалуйста, введите целое число.")
-
-while True:
-    try:
-        fi2 = int(input("Введите значение φ2 : "))
-        break
-    except ValueError:
-        print("Вы ввели некорректное значение. Пожалуйста, введите целое число.")
+r1=pr('R1')
+r2=pr('R2')
+fi1=pr2('φ1')
+fi2=pr2('φ2')
 
 def d(r1, r2, fi1, fi2):
     return (r1**2 + r2**2 - 2 * r1 * r2 * math.cos((fi1-fi2)))**0.5
 
 print('Длина отрезка на плоскости при заданных полярных координатах: ', d(r1, r2, fi1, fi2))
-
